51df0bd8ec72dc3724a7acc07cb9d3a6eb406ae2
johntran <tranjohn1211@gmail.com>
2022-10-21 15:07:49 -0700

    most up to date files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
+import Header from './components/Header';
+import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Customers from './pages/Customers';
+import Dictionary from './pages/Dictionary';
+import Definition from './pages/Definition';
+import NotFound from './components/NotFound';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header>
+                <Routes>
+                    <Route path="/employees" element={<Employees />} />
+                    <Route path="/dictionary" element={<Dictionary />} />
+                    <Route
+                        path="/dictionary/:search"
+                        element={<Definition />}
+                    />
+                    <Route path="/customers" element={<Customers />} />
+                    <Route path="/404" element={<NotFound />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Header>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Customers.jsx
@@ -0,0 +1,5 @@
+const Customers = () => {
+    return ( <div>Hello this is where the customers goes</div> );
+}
+ 
+export default Customers;
