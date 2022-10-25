c605292dd0c6b68a35dbb77e55909a73be3ac266
johntran <tranjohn1211@gmail.com>
2022-10-24 17:45:22 -0700

    create a backend for detail

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,5 +1,22 @@
-const Customers = () => {
-    return ( <div>Hello this is where the customers goes</div> );
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+export default function Customers() {
+    const [customers, setCustomers] = useState()
+    useEffect(() => {
+        console.log("Fetching...")
+        fetch('http://localhost:8000/api/customers/')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setCustomers(data.customers)
+        })
+    },[]);
+    return (
+        <>
+        <h1>Here are our customer: </h1>
+        {customers ? customers.map((customer) => {
+            return <p><Link to={"/customers/" + customer.id}>{customer.name}</Link></p>
+        }): null}
+        </>
+    )
 }
- 
-export default Customers;
